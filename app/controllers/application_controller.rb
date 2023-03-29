1197ca2a82faee39324f37246bd9b3534e7823b4
MikhailBogun <booozyby@gmail.com>
2023-03-29 14:02:49 +0300

    Add controller to user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,19 @@
 class ApplicationController < ActionController::API
+  before_action :set_default_response_format
+
+  # include SimpleEndpoint::Controller
+  # include DefaultEndpoint
+  # include JWTSessions::RailsAuthorization
+
+  # rescue_from JWTSessions::Errors::Unauthorized, with: :not_authorized
+
+  # private
+
+  # def not_authorized
+  #   head(:unauthorized)
+  # end
+
+  def set_default_response_format
+    request.format = :json unless params[:format]
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  namespace :api, defaults: { format: :json } do
+    namespace :v1 do
+      # resources :registration, only: %i[create destroy]
+      resource :user, only: %i[show]
+      # resource :session, only: %i[create destroy]
+    end
+  end
 end
